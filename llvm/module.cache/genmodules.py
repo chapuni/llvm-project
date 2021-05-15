3a0b648e09477d8b8c9f2082b2921fdfbb6fc391
NAKAMURA Takumi <geek4civic@gmail.com>
2021-05-15 22:52:36 +0900

    genmodules.py: ファイルが存在しないのあかん

--- llvm/module.cache/genmodules.py
+++ llvm/module.cache/genmodules.py
@@ -98,11 +98,12 @@ set_target_properties(%s PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BIN
         for (mod,pcm) in mods.items():
             cfn = fn(mod)
             line = "#pragma clang module import %s\n" % mod
-            with open("%s" % cfn) as f:
-                if line==f.read():
-                    line = ""
+            if os.path.exists(cfn):
+                with open(cfn) as f:
+                    if line==f.read():
+                        line = ""
             if line != "":
-                with open("%s" % cfn, "w") as f:
+                with open(cfn, "w") as f:
                     f.write(line)
             cmake.write("set_source_files_properties(%s PROPERTIES COMPILE_OPTIONS -fmodule-name=%s OBJECT_OUTPUTS ${moddir}/%s)\n" % (cfn, mod, pcm))
         cmake.write("add_dependencies(%s anchor_all)\n" % mn)
